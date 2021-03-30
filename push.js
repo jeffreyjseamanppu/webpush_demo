a497087ff342bf4e20b629980b80b8d917f274c6
LandShark <jseaman@pointpark.edu>
2021-03-30 19:36:41 -0400

    Added push js

---
+++ push.js
@@ -0,0 +1,15 @@
+var push = require('web-push');
+
+//let vapidkeys = push.generateVAPIDKeys()
+//console.log(vapidkeys);
+
+let vapidkeys = {
+    publickey: 'BNgHZEeaNeI4M9c4r1FaMkUSLI2rKOBWcJGw-mGh2t5lHBjJngaHJJAXzMhlrqVlgNflL4Fa1MEhi93BM5DPHWk',
+    privatekey: 'DQAG1k5wo5GZ6WX1ChXK0C7cF0JDxwRzFFgIjnxKDT4'
+}
+
+push.setVapidDetails('mailto:jseaman@pointpark.edu', vapidkeys.publickey, vapidkeys.privatekey)
+
+let sub = {};
+
+push.sendNotification(sub, 'Welcome to Friday');
